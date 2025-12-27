7b7a05116cce94cb50baf1ff07825ab2260c6c0a
SangHo-Daniel-Kim <sanghoisworking@gmail.com>
2025-12-27 20:53:21 +0000

    Deploying to gh-pages from @ SangHo-Daniel-Kim/SangHo-Daniel-Kim.github.io@16f680cfe761e6f7885e4ec9dddec2dfc64a2031 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "SangHo-Daniel-Kim/SanghoKim.github.io",
+    "data-repo": "SangHo-Daniel-Kim/SangHo-Daniel-Kim.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
